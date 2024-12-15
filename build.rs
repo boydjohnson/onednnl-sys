c5ad85db38b8f4159cff642c8e7ae26a9f33238e
Boyd Johnson <johnson.boyd@gmail.com>
2024-12-15 15:38:08 +0000

    Remove build strings that already match regex

--- build.rs
+++ build.rs
@@ -5,11 +5,6 @@ fn main() {
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-        .constified_enum_module("dnnl_accumulation_mode_t")
-        .constified_enum_module("dnnl_alg_kind_t")
-        .constified_enum_module("dnnl_cpu_isa_hints_t")
-        .constified_enum_module("dnnl_cpu_isa_t")
-        .constified_enum_module("dnnl_data_type_t")
         .constified_enum_module("dnnl_.*")
         .generate()
         .unwrap();
